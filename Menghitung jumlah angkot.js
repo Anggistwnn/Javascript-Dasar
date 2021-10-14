d9cf38c1e8a4ec89bd4cd7e13d520c2916f0c0b2
Anggistwnn <anggistwnn@gmail.com>
2021-10-14 14:00:16 +0700

    Menghitung jumlah angkot yang beroperasi menggunakan console

---
+++ Menghitung jumlah angkot.js
@@ -0,0 +1,8 @@
+var jmlAngkot = 10;
+    var angkotBeroperasi = 6;
+    var noAngkot = 1;
+
+    while (noAngkot <=  angkotBeroperasi){
+        console.log('Angkot No. '+ noAngkot +' beroperasi dengan baik.');
+    noAngkot++;
+    }
