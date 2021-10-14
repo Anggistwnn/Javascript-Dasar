54a43c0072c10768098ffcd7a5715b321a6b7e10
Anggistwnn <anggistwnn@gmail.com>
2021-10-14 13:48:59 +0700

    Looping Welcoem

---
+++ Looping Welcome.js
@@ -0,0 +1,10 @@
+alert('selamat datang..');
+var lagi = true;
+
+while (lagi === true ) {
+    var nama = prompt('masukan nama:')
+    alert('halo ' + nama);
+
+    lagi = confirm('coba lagi?')
+}
+    alert('terima kasih..')
