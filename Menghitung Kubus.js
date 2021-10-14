b78b1e9ec01799323b0427cdb9bc00c045deeae3
Anggistwnn <anggistwnn@gmail.com>
2021-10-14 13:39:57 +0700

    Mengitung Kubus

---
+++ Menghitung Kubus.js
@@ -0,0 +1,12 @@
+function jumlahVolumeKubus(a, b) {
+    return a + b;
+}
+
+function kali(a, b) {
+    return a * b;
+}
+
+var hasil = kali(jumlahVolumeKubus(2, 1), jumlahVolumeKubus(5, 3));
+
+
+console.log(hasil);
