31dca08931c35b31e2432cd5520de3386839de2e
Anggistwnn <anggistwnn@gmail.com>
2022-03-22 02:06:14 +0700

    DOM Manipulation Event

--- DOM Selection.js/script.js
+++ DOM Selection.js/script.js
@@ -98,36 +98,78 @@
 
 // DOM Manipulation Part 2
 // membuat element baru
-const pBaru = document.createElement('p');
-const teksPBaru = document.createTextNode('Paragraf Baru');
+// const pBaru = document.createElement('p');
+// const teksPBaru = document.createTextNode('Paragraf Baru');
 // menyimpan yang diatas kedalam element baru, karna mereka belum terimplementasi pada tampilan hanya ada pada memory saja
-pBaru.appendChild(teksPBaru);
+// pBaru.appendChild(teksPBaru);
 // simpan PBaru di akhir section A, menggunakan code diatas
 
 // code dibawah menambahkan item diatara item pada section B, mengguanakan insert before yang juga menggunakan pseudo class css nth-child 
-const sectionA = document.getElementById('a');
-sectionA.appendChild(pBaru);
+// const sectionA = document.getElementById('a');
+// sectionA.appendChild(pBaru);
 
-const liBaru = document.createElement('li');
-const teksLiBaru = document.createTextNode('Item Baru');
-liBaru.appendChild(teksLiBaru);
+// const liBaru = document.createElement('li');
+// const teksLiBaru = document.createTextNode('Item Baru');
+// liBaru.appendChild(teksLiBaru);
 
-const ul = document.querySelector('section#b ul');
-const li2 = document.querySelector('li:nth-child(2)');
+// const ul = document.querySelector('section#b ul');
+// const li2 = document.querySelector('li:nth-child(2)');
 
-ul.insertBefore(liBaru, li2);
+// ul.insertBefore(liBaru, li2);
 
 // menghilangkan element menggunakan removeChild
-const link = document.getElementsByTagName('a')[0];
-sectionA.removeChild(link);
+// const link = document.getElementsByTagName('a')[0];
+// sectionA.removeChild(link);
 
 //code dibawah mengganti p4 menjadi element baru menggunakan replaceChild
-const sectionB = document.getElementById('b');
-const p4 = sectionB.querySelector('p');
+// const sectionB = document.getElementById('b');
+// const p4 = sectionB.querySelector('p');
 
-const h2Baru = document.createElement('h2');
-const teksh2Baru = document.createTextNode('Judul Baru!');
+// const h2Baru = document.createElement('h2');
+// const teksh2Baru = document.createTextNode('Judul Baru!');
 
-h2Baru.appendChild(teksh2Baru);
+// h2Baru.appendChild(teksh2Baru);
 
-sectionB.replaceChild(h2Baru, p4);
+// sectionB.replaceChild(h2Baru, p4);
+
+// const p3 = document.querySelector('.p3');
+
+// function ubahWarnaP3() {
+//     p3.style.backgroundColor = 'lightblue';
+// }
+
+// function ubahWarnaP2() {
+//     p2.style.backgroundColor = 'lightblue';
+// }
+// const p2 = document.querySelector('.p2');
+// p2.onclick = ubahWarnaP2;
+
+// const p4 = document.querySelector('section#b p');
+
+// p4.addEventListener('click', function () {
+//     // Ambil Parent
+//     const ul = document.querySelector('section#b ul');
+//     // membuat element baru
+//     const liBaru = document.createElement('li');
+//     const teksLiBaru = document.createTextNode('Item Baru');
+//     // merangkainya dan siap ditampilkan
+//     liBaru.appendChild(teksLiBaru);
+//     // dan kita simpan didalam ul
+//     ul.appendChild(liBaru);
+// });
+
+// const p3 = document.querySelector('.p3');
+// dibawah menggunakan EVENT HANDLER
+// p3.onclick = function () {
+//     p3.style.backgroundColor = 'Coral';
+// }
+// p3.onclick = function () {
+//     p3.style.color = 'lightgreen';
+// }
+// dan dibawah ini mengguanakn EVENT LISTENER
+// p3.addEventListener('mouseenter', function () {
+//     p3.style.backgroundColor = 'CORAL';
+// });
+// p3.addEventListener('mouseleave', function () {
+//     p3.style.backgroundColor = 'red';
+// });
