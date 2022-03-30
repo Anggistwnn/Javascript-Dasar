a13b9fc9e3adce9814f986b1728545c1575bc561
Anggistwnn <anggistwnn@gmail.com>
2022-03-30 11:09:14 +0700

    DOM Traversal - Event Bubbing

--- DOM Traversal.js/script.js
+++ DOM Traversal.js/script.js
@@ -16,7 +16,7 @@
 // -----------------------------------------------------------------------
 // DOM Traversal
 // menangkap elementnya
-const close = document.querySelectorAll('.close');
+// const close = document.querySelectorAll('.close');
 // menggunakan looping [i] dalam menangkap semua card
 // for (let i = 0; i < close.length; i++) {
 //     close[i].addEventListener('click', function () {
@@ -28,27 +28,43 @@ const close = document.querySelectorAll('.close');
 // -----------------------------------------------------------------------------
 // menggunakan forEach
 // el berguna untuk mengambil 1 element sama dengan mengambil close[i];
-close.forEach(function (el) {
-    el.addEventListener('click', function (e) {
-        // menargetkan element yang akan di hapus yaitu semua yang ada pada cardnya
-        // jika tidak menggunakan parentElement hanya akan menghilangkan si tombol x nya saja 
-        e.target.parentElement.style.display = 'none';
-        // Prevent Default adalah untunk menghentikan aksi default dari sebuah element. dalam praktek ini saya menghilangkan aksi default dari href yang ditempatkan pada button close
-        e.preventDefault();
-    })
-});
+// close.forEach(function (el) {
+//     el.addEventListener('click', function (e) {
+// menargetkan element yang akan di hapus yaitu semua yang ada pada cardnya
+// jika tidak menggunakan parentElement hanya akan menghilangkan si tombol x nya saja 
+// e.target.parentElement.style.display = 'none';
+// Prevent Default adalah untunk menghentikan aksi default dari sebuah element. dalam praktek ini saya menghilangkan aksi default dari href yang ditempatkan pada button close
+//         e.preventDefault();
+//         e.stopPropagation();
+//     });
+// });
 
 
 
-const nama = document.querySelector('.nama');
+// const nama = document.querySelector('.nama');
 // mengambil parent(ibu) dari element nama yaitu card
-console.log(nama.parentElement);
+// console.log(nama.parentElement);
 // mengambil parent kakek dari element nama yaitu container
-console.log(nama.parentElement.parentElement);
+// console.log(nama.parentElement.parentElement);
 // mencari sodara kandung dari parent(keluarga 'card') yang sama milik (nama)
 // jika menggunakan .nextSibling yang diambil adalah nodenya (enter) pun akan dibaca olehnya oleh karna itu kita harus menggunakan .nextElementSibling yang pasti akan menggambil element bukan node
-console.log(nama.nextElementSibling);
+// console.log(nama.nextElementSibling);
 // ----------------------------------------------------
 
+// event bubbling sama dengan prevent tetapi jika bubbling berlaku untuk semua element pembungkusnya
+// pada function dibawah adalah contohnya, dan akan terus berlaku sampai element pembungkusnya habis /body
+// const card = document.querySelectorAll('.card');
 
-// Prevent Default adalah untunk menghentikan aksi default dari seb uah element
+// card.forEach(function (card) {
+//     card.addEventListener('click', function (e) {
+//         alert('Okee!')
+//     });
+// });
+// membuat event bubbling ini tidak berfungsi untuk element closenya, menggunakan stoppropagation diletakan di akhir script
+
+const container = document.querySelector('.container');
+container.addEventListener('click', function (e) {
+    if (e.target.className == 'close') {
+        e.target.parentElement.style = 'none';
+    }
+})
