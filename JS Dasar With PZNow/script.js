e0a175496ae9d7b483590492e556ded589eaa003
Anggistwnn <anggistwnn@gmail.com>
2022-07-07 23:23:07 +0700

    Menambahkan file latihan JS dasar

--- JS Dasar With PZNow/script.js
+++ JS Dasar With PZNow/script.js
@@ -1,1 +1,14 @@
-document.writeln("Hello World!");
+// document.writeln("Hello World!");
+// document.writeln("<textarea cols='100' rows='10'");
+document.writeln("Anggi Setiawan!");
+document.writeln("</br>");
+document.writeln("Anggi Setiawan!");
+document.writeln("</br>");
+document.writeln("Anggi Setiawan!");
+document.writeln("</br>");
+document.writeln("Anggi Setiawan!");
+document.writeln("</br>");
+document.writeln("Anggi Setiawan!");
+document.writeln("</br>");
+document.writeln("Anggi Setiawan!");
+// document.writeln("</textarea>")
