4ff974a00eaf4f37b4683ac62d6902a31231e1e9
Anggistwnn <anggistwnn@gmail.com>
2022-07-08 17:46:56 +0700

    menambahkan file latihan JS dasar

--- JS Dasar With PZNow/script.js
+++ JS Dasar With PZNow/script.js
@@ -1,14 +1,28 @@
 // document.writeln("Hello World!");
 // document.writeln("<textarea cols='100' rows='10'");
-document.writeln("Anggi Setiawan!");
-document.writeln("</br>");
-document.writeln("Anggi Setiawan!");
-document.writeln("</br>");
-document.writeln("Anggi Setiawan!");
-document.writeln("</br>");
-document.writeln("Anggi Setiawan!");
-document.writeln("</br>");
-document.writeln("Anggi Setiawan!");
-document.writeln("</br>");
-document.writeln("Anggi Setiawan!");
+
+// Mengakses Nama variable 
+// jangan lagi menggunakan var karna sudah diperbaharui di ES6
+// 
+
+// let mahasiswa1 = "Roudohh"
+// let mahasiswa2 = "Anggi Setiawan"
+
+
+// document.writeln(mahasiswa1);
+// document.writeln("</br>");
+// document.writeln(mahasiswa2);
+// document.writeln("</br>");
+// document.writeln(mahasiswa1);
+// document.writeln("</br>");
+// document.writeln(mahasiswa2);
+// document.writeln("</br>");
+// document.writeln(mahasiswa1);
+// document.writeln("</br>");
+// document.writeln(mahasiswa2);
 // document.writeln("</textarea>")
+
+
+const belajar = "Belajar Adalah cara saya untuk bertahan hidup";
+
+belajar = "malas belajar";
