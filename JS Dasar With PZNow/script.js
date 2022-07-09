a961f7e297f34b6254667c2f2a561fb32c603ff5
Anggistwnn <anggistwnn@gmail.com>
2022-07-09 08:39:10 +0700

    menambahkan file latihan aritmatika pada JS

--- JS Dasar With PZNow/script.js
+++ JS Dasar With PZNow/script.js
@@ -1,9 +1,9 @@
 // document.writeln("Hello World!");
 // document.writeln("<textarea cols='100' rows='10'");
 
-// Mengakses Nama variable 
+// Mengakses Nama variable
 // jangan lagi menggunakan var karna sudah diperbaharui di ES6
-// 
+//
 
 // let mahasiswa1 = "Roudohh"
 // let mahasiswa2 = "Anggi Setiawan"
@@ -23,6 +23,21 @@
 // document.writeln("</textarea>")
 
 
-const belajar = "Belajar Adalah cara saya untuk bertahan hidup";
+// const merupakan deklarasi variable yang tidak dapat diganti2, yang memiliki arti juga constant
+// const belajar = "Belajar Adalah cara saya untuk bertahan hidup";
+// belajar = "malas belajar";
 
-belajar = "malas belajar";
+// operasi aritmatika pada JS
+
+let result = 1 + 2;
+document.writeln(`<p> 1 + 2 = ` + result + ` </p>`)
+let originalResult = result;
+
+result = result - 1;
+document.writeln(`<h1>` + originalResult + ` - 1 = ` + result + `</h1>`);
+originalResult = result;
+
+result = result + 3;
+document.writeln(`<h2>` + originalResult + ` + 3 = ` + result + `</h2>`)
+
+
